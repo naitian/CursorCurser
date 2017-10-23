8493cad848dd3a4c9745e27e25b7c9454da53e7b
Naitian Zhou <nzhou2011@gmail.com>
2017-10-22 20:24:12 -0400

    Formatting

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName('body')[0];
 const curseAr = [
-
-"Frazzlin, dadgummit!",
-"Danabbit!",
-"Fiddlesticks!",
-"Holy land! Man Horse!",
-"Shiznits",
-"Mother of pearl!",
-"Welllll darn.",
-"Blimey!",
-"Bollocks..."
-
+    'Frazzlin, dadgummit!',
+    'Danabbit!',
+    'Fiddlesticks!',
+    'Holy land! Man Horse!',
+    'Shiznits',
+    'Mother of pearl!',
+    'Welllll darn.',
+    'Blimey!',
+    'Bollocks...'
 ];
- 
+
 console.log(curseAr);
-body.addEventListener("click", () => {
-	body.innerHTML = "<div id='curseoverlay'><span id='cursetext'>" + curseAr[Math.floor(Math.random() * curseAr.length)] + "</span></div>" + body.innerHTML;
-	window.setTimeout(() => {
-		ol = document.getElementById("curseoverlay");
-		ol.parentNode.removeChild(ol);
-	}, 200);
+body.addEventListener('click', () => {
+    body.innerHTML = '<div id="curseoverlay"><span id="cursetext">' +
+        curseAr[Math.floor(Math.random() * curseAr.length)] +
+        '</span></div>' + body.innerHTML;
+    window.setTimeout(() => {
+        ol = document.getElementById('curseoverlay');
+        ol.parentNode.removeChild(ol);
+    }, 200);
 })
